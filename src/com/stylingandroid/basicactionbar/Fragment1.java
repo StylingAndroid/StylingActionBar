3efb17c0355a2a6f37d1d62bbf965805191bd5d5
Mark Allison <mark@stylingandroid.com>
2012-07-28 16:39:32 +0100

    Added code for Tab navigation

---
+++ src/com/stylingandroid/basicactionbar/Fragment1.java
@@ -0,0 +1,17 @@
+package com.stylingandroid.basicactionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment
+{
+	@Override
+	public View onCreateView( LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState )
+	{
+		return inflater.inflate( R.layout.frag1, container, false );
+	}
+}
--- src/com/stylingandroid/basicactionbar/MainActivity.java
+++ src/com/stylingandroid/basicactionbar/MainActivity.java
@@ -1,18 +1,72 @@
 package com.stylingandroid.basicactionbar;
 
-import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity
 {
 
+	private class MyTabListener implements ActionBar.TabListener
+	{
+		private Fragment mFragment;
+		private final Activity mActivity;
+		private final String mFragName;
+
+		public MyTabListener( Activity activity, String fragName )
+		{
+			mActivity = activity;
+			mFragName = fragName;
+		}
+
+		@Override
+		public void onTabReselected( Tab tab, FragmentTransaction ft )
+		{
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onTabSelected( Tab tab, FragmentTransaction ft )
+		{
+			mFragment = Fragment.instantiate( mActivity, mFragName );
+			ft.add( android.R.id.content, mFragment );
+		}
+
+		@Override
+		public void onTabUnselected( Tab tab, FragmentTransaction ft )
+		{
+			ft.remove( mFragment );
+			mFragment = null;
+		}
+	}
+
 	@Override
 	public void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
-		setContentView( R.layout.main );
+
+		ActionBar ab = getActionBar();
+		ab.setNavigationMode( ActionBar.NAVIGATION_MODE_TABS );
+		
+		Tab tab = ab.newTab()
+				.setText( R.string.frag1 )
+				.setTabListener( 
+						new MyTabListener( this, 
+								Fragment1.class.getName() ) );
+		ab.addTab( tab );
+
+		tab = ab.newTab()
+				.setText( R.string.frag2 )
+				.setTabListener( 
+						new MyTabListener( this, 
+								Fragment2.class.getName() ) );
+		ab.addTab( tab );
 	}
 
 	@Override
@@ -30,8 +84,7 @@ public class MainActivity extends Activity
 		{
 			// Handle Settings
 			ret = true;
-		} 
-		else
+		} else
 		{
 			ret = super.onOptionsItemSelected( item );
 		}
