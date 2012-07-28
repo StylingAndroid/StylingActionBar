dca3aaa50f1241e436118a2a01ba45dd70296929
Mark Allison <mark@stylingandroid.com>
2012-07-28 14:02:05 +0100

    Initial commit

---
+++ src/com/stylingandroid/basicactionbar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.stylingandroid.basicactionbar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
