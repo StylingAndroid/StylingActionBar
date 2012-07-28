f719a7a7807b5520b5216bf2f2cb46d651ae81b8
Mark Allison <mark@stylingandroid.com>
2012-07-28 15:06:59 +0100

    Added basic action handler

--- src/com/stylingandroid/basicactionbar/MainActivity.java
+++ src/com/stylingandroid/basicactionbar/MainActivity.java
@@ -3,18 +3,38 @@ package com.stylingandroid.basicactionbar;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.main );
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu( Menu menu )
+	{
+		getMenuInflater().inflate( R.menu.main, menu );
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected( MenuItem item )
+	{
+		boolean ret;
+		if (item.getItemId() == R.id.menu_settings)
+		{
+			// Handle Settings
+			ret = true;
+		} 
+		else
+		{
+			ret = super.onOptionsItemSelected( item );
+		}
+		return ret;
+	}
 }
